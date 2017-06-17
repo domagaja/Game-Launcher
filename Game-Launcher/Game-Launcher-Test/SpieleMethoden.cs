f4a8dc92ff9ac954e0a951015697347813327c3f
domagaja <domagaja@live.de>
2017-06-17 18:37:17 +0200

    V1

---
+++ Game-Launcher/Game-Launcher-Test/SpieleMethoden.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Game_Launcher_Test
+{
+    internal class ParameterDesSpiels
+    {
+       public string TitelDesSpiels { get; set; }
+       public string InstallationsDatum { get; set; }
+       public string ZuletztGespielt { get; set; }
+       public string InstallationsPfad { get; set; }
+       public string Kategorie { get; set; }
+       public string Publisher { get; set; }
+       public int UskEinstufung { get; set; }
+    }
+    internal class SpieleMethoden
+    {
+        public List<ParameterDesSpiels> ParameterDesSpielsListe;
+
+
+        public SpieleMethoden()
+        {
+            
+        }
+
+        internal void SpielHinzufügen(string Titel, string Install_Datum, string Zuletzt_Gespielt, string Install_Pfad, string kategorie, string publisher, int Usk_Einstufung)
+        {
+            if (Titel == null || Install_Datum == null || Zuletzt_Gespielt == null || Install_Pfad == null || kategorie == null|| publisher == null || Usk_Einstufung != 0  && Usk_Einstufung != 6 && Usk_Einstufung != 12 && Usk_Einstufung != 16 && Usk_Einstufung != 18)
+            {
+                throw new ArgumentNullException("Bruh, es gibt nichts zum Hinzufügen.");
+            }
+            ParameterDesSpielsListe = new List<ParameterDesSpiels>();
+            ParameterDesSpielsListe.Add(new ParameterDesSpiels()
+            {
+                TitelDesSpiels = Titel,
+                InstallationsDatum = Install_Datum,
+                ZuletztGespielt = Zuletzt_Gespielt,
+                InstallationsPfad = Install_Pfad,
+                Kategorie = kategorie,
+                Publisher = publisher,
+                UskEinstufung = Usk_Einstufung
+            });
+        }
+    }
+}
--- Game-Launcher/Game-Launcher-Test/UnitTest1.cs
+++ Game-Launcher/Game-Launcher-Test/UnitTest1.cs
@@ -9,6 +9,15 @@ namespace Game_Launcher_Test
         [TestMethod]
         public void TestMethod1()
         {
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void No_Game_Added_Throws_ArgumentNullException_TestMethod()
+        {
+            var spielhinzufügen = new SpieleMethoden();
+            spielhinzufügen.SpielHinzufügen("GTA 5","17.06.2017","18.06.2017","Hier","MOBA","PhilippGames",1);
+            Assert.AreEqual(spiel, "GTA 5", "17.06.2017", "18.06.2017", "Hier", "MOBA", "PhilippGames", 1);
         }
     }
 }
